1121add6cdf7fa55ea64659266d38f559e305e34
Dmitrii Trofimov <probcf@gmail.com>
2021-07-22 17:13:27 +0300

    Lesson_21 IO_NIO FileReader

---
+++ src/work_with_files/FileReaderExp.java
@@ -0,0 +1,23 @@
+package work_with_files;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderExp {
+	public static void main(String[] args) throws IOException {
+		FileReader reader = null;
+		try {
+			reader = new FileReader("C:\\Users\\Дмитрий\\Desktop\\text1.txt");
+			int character;
+			while ((character = reader.read()) != -1) {
+				System.out.print((char) character);
+			}
+			System.out.println();
+			System.out.println("Done!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			reader.close();
+		}
+	}
+}

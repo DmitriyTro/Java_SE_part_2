dfeb0fa521b817b2cd4fea47cc70c9ff46e9daf3
Dmitrii Trofimov <probcf@gmail.com>
2021-07-22 16:26:15 +0300

    Lesson_21 IO_NIO FileWriter

---
+++ src/work_with_files/FileWriterExp.java
@@ -0,0 +1,29 @@
+package work_with_files;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterExp {
+	public static void main(String[] args) throws IOException {
+		String rubai = "Много лет размышлял я над жизнью земной.\n" +
+				"Непонятного нет для меня под луной.\n" +
+				"Мне известно, что мне ничего не известно!\n" +
+				"Вот последняя правда, открытая мной.\n";
+		String s = "Hello";
+
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter("C:\\Users\\Дмитрий\\Desktop\\text1.txt", true);
+//			for (int i = 0; i < rubai.length(); i++) {
+//				writer.write(rubai.charAt(i));
+//			}
+//			writer.write(rubai);
+			writer.write(s);
+			System.out.println("Done!");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			writer.close();
+		}
+	}
+}

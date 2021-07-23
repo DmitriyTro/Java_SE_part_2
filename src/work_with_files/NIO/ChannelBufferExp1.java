a3ca96117c85d844671365670afdea5fabb9d08e
Dmitrii Trofimov <probcf@gmail.com>
2021-07-23 19:16:34 +0300

    Lesson_21 IO_NIO Buffer and Channel (reading)

---
+++ src/work_with_files/NIO/ChannelBufferExp1.java
@@ -0,0 +1,39 @@
+package work_with_files.NIO;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class ChannelBufferExp1 {
+	public static void main(String[] args) {
+
+		try (RandomAccessFile file = new RandomAccessFile(
+				"test10.txt", "rw");
+		     FileChannel channel = file.getChannel();) {
+
+			ByteBuffer buffer = ByteBuffer.allocate(25);
+			StringBuilder stih = new StringBuilder();
+
+			int byteRead = channel.read(buffer);
+			while (byteRead > 0) {
+				System.out.println("Read " + byteRead);
+
+				buffer.flip();
+
+				while (buffer.hasRemaining()) {
+					stih.append((char) buffer.get());
+				}
+
+				buffer.clear();
+				byteRead = channel.read(buffer);
+			}
+			System.out.println(stih);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}

16f1c6798acc07b389b15a84c5fe13c69f96b931
Dmitrii Trofimov <probcf@gmail.com>
2021-07-23 19:31:33 +0300

    Lesson_21 IO_NIO Buffer and Channel (writing)

--- src/work_with_files/NIO/ChannelBufferExp1.java
+++ src/work_with_files/NIO/ChannelBufferExp1.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 
 public class ChannelBufferExp1 {
 	public static void main(String[] args) {
@@ -30,6 +31,19 @@ public class ChannelBufferExp1 {
 				byteRead = channel.read(buffer);
 			}
 			System.out.println(stih);
+
+			String text = "\nThere are only two ways to live your life." +
+					" One is as though nothing is a miracle. The other is as" +
+					" though everything is a miracle.";
+
+//			ByteBuffer byteBuffer2 = ByteBuffer.allocate(text.getBytes().length);
+//			byteBuffer2.put(text.getBytes());
+//			byteBuffer2.flip();
+//			channel.write(byteBuffer2);
+
+			ByteBuffer buffer3 = ByteBuffer.wrap(text.getBytes());
+			channel.write(buffer3);
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {

e8e3ffe7ea9d1bfa03a1fc34fb99fb4815d1392b
Dmitrii Trofimov <probcf@gmail.com>
2021-07-25 18:01:09 +0300

    Lesson_21 RegEx part_2

--- src/regex/RegexExp1.java
+++ src/regex/RegexExp1.java
@@ -1,5 +1,8 @@
 package regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexExp1 {
 	public static void main(String[] args) {
 		String s =
@@ -9,5 +12,36 @@ public class RegexExp1 {
 						" email: masha@yandex.ru, Postcode: UKR54, Phone Number: +987654321;" +
 						" Chuck Norris, USA, Hollywood, All stars street, 87, Flat 21," +
 						" email: chuck@gmail.com, Postcode: USA23, Phone Number: +136478952.";
+
+//		Pattern pattern = Pattern.compile("\\b\\d{2}\\b");
+//		Pattern pattern = Pattern.compile("\\+\\d{9}");
+//		Pattern pattern = Pattern.compile("\\w+@\\w+\\.(ru|com)");
+//		String s1 = "poka abc       Zaur  dom   kino  abdffewew";
+//		Pattern pattern = Pattern.compile("\\w\\s+\\w");
+//		String s1 = "abcd abc3 abd78ffewew";
+//		Pattern pattern = Pattern.compile("\\D{2,6}");
+
+//		String s1 = "ABCABABCABC";
+//		Pattern pattern = Pattern.compile("(AB){2,3}");
+
+//		String s1 = "ABCABABCABCDABABABDDABBAD";
+//		Pattern pattern = Pattern.compile("D(AB){2,3}");
+
+//		String s1 = "ABCABABCABCDABABABDDABBAD";
+//		Pattern pattern = Pattern.compile("D(AB)?");
+
+//		String s1 = "ABCABABCABCDABABABDDABBAD";
+//		Pattern pattern = Pattern.compile("D(AB)*");
+
+//		String s1 = "abcd adce adcfaabcgabc";
+//		Pattern pattern = Pattern.compile("\\Aabcd");
+
+		String s1 = "abcd adce adcfaabcgabc";
+		Pattern pattern = Pattern.compile("abc\\z");
+		Matcher matcher = pattern.matcher(s1);
+
+		while (matcher.find()) {
+			System.out.println("Position: " + matcher.start() + "   " + matcher.group());
+		}
 	}
 }
